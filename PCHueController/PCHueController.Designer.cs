40401da963f67e5ccff378d9b7e9bdb9e98bab94
Jstanford5216 <jstanford5216@gmail.com>
2018-07-26 08:17:05 +0100

    Got basic colors working and colour loop.

--- PCHueController/PCHueController.Designer.cs
+++ PCHueController/PCHueController.Designer.cs
@@ -28,49 +28,231 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PCHueController));
+            this.pnlCurrentSwatch = new System.Windows.Forms.Panel();
+            this.lblCurrent = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.lstHubs = new System.Windows.Forms.ListBox();
+            this.lblTitle1 = new System.Windows.Forms.Label();
+            this.btnNewColour = new System.Windows.Forms.Button();
+            this.lblTitle2 = new System.Windows.Forms.Label();
+            this.lblTitle3 = new System.Windows.Forms.Label();
+            this.txtInputKey = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.grpConnection = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnColorloop = new System.Windows.Forms.Button();
+            this.btnOceanM = new System.Windows.Forms.Button();
+            this.grpConnection.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // pnlCurrentSwatch
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Location = new System.Drawing.Point(75, 37);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(86, 60);
-            this.panel1.TabIndex = 0;
+            this.pnlCurrentSwatch.BackColor = System.Drawing.Color.White;
+            this.pnlCurrentSwatch.Location = new System.Drawing.Point(623, 35);
+            this.pnlCurrentSwatch.Name = "pnlCurrentSwatch";
+            this.pnlCurrentSwatch.Size = new System.Drawing.Size(86, 60);
+            this.pnlCurrentSwatch.TabIndex = 0;
             // 
-            // label1
+            // lblCurrent
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label1.Location = new System.Drawing.Point(44, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(146, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Current Colour:";
+            this.lblCurrent.AutoSize = true;
+            this.lblCurrent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblCurrent.ForeColor = System.Drawing.Color.White;
+            this.lblCurrent.Location = new System.Drawing.Point(609, 12);
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(116, 20);
+            this.lblCurrent.TabIndex = 0;
+            this.lblCurrent.Text = "Current Colour:";
             // 
-            // button1
+            // btnRegister
             // 
-            this.button1.Location = new System.Drawing.Point(213, 50);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Select new colour";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnRegister.ForeColor = System.Drawing.Color.White;
+            this.btnRegister.Location = new System.Drawing.Point(36, 282);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(93, 37);
+            this.btnRegister.TabIndex = 2;
+            this.btnRegister.Text = "Get API Key";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
             // 
-            // Form1
+            // lstHubs
+            // 
+            this.lstHubs.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.lstHubs.ForeColor = System.Drawing.Color.White;
+            this.lstHubs.FormattingEnabled = true;
+            this.lstHubs.Location = new System.Drawing.Point(16, 66);
+            this.lstHubs.Name = "lstHubs";
+            this.lstHubs.Size = new System.Drawing.Size(262, 95);
+            this.lstHubs.TabIndex = 3;
+            this.lstHubs.SelectedIndexChanged += new System.EventHandler(this.lstHubs_SelectedIndexChanged);
+            // 
+            // lblTitle1
+            // 
+            this.lblTitle1.AutoSize = true;
+            this.lblTitle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblTitle1.ForeColor = System.Drawing.Color.White;
+            this.lblTitle1.Location = new System.Drawing.Point(32, 36);
+            this.lblTitle1.Name = "lblTitle1";
+            this.lblTitle1.Size = new System.Drawing.Size(222, 20);
+            this.lblTitle1.TabIndex = 4;
+            this.lblTitle1.Text = "1.Discover and select a device";
+            // 
+            // btnNewColour
+            // 
+            this.btnNewColour.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnNewColour.ForeColor = System.Drawing.Color.White;
+            this.btnNewColour.Location = new System.Drawing.Point(73, 66);
+            this.btnNewColour.Name = "btnNewColour";
+            this.btnNewColour.Size = new System.Drawing.Size(116, 23);
+            this.btnNewColour.TabIndex = 1;
+            this.btnNewColour.Text = "Generic colour picker";
+            this.btnNewColour.UseVisualStyleBackColor = true;
+            this.btnNewColour.Click += new System.EventHandler(this.btnNewColour_Click);
+            // 
+            // lblTitle2
+            // 
+            this.lblTitle2.AutoSize = true;
+            this.lblTitle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblTitle2.ForeColor = System.Drawing.Color.White;
+            this.lblTitle2.Location = new System.Drawing.Point(52, 187);
+            this.lblTitle2.Name = "lblTitle2";
+            this.lblTitle2.Size = new System.Drawing.Size(190, 40);
+            this.lblTitle2.TabIndex = 7;
+            this.lblTitle2.Text = "2.Get API key or connect \r\nwith an existing key";
+            // 
+            // lblTitle3
+            // 
+            this.lblTitle3.AutoSize = true;
+            this.lblTitle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblTitle3.ForeColor = System.Drawing.Color.White;
+            this.lblTitle3.Location = new System.Drawing.Point(43, 36);
+            this.lblTitle3.Name = "lblTitle3";
+            this.lblTitle3.Size = new System.Drawing.Size(174, 20);
+            this.lblTitle3.TabIndex = 8;
+            this.lblTitle3.Text = "3.Choose Colour/Effect";
+            // 
+            // txtInputKey
+            // 
+            this.txtInputKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtInputKey.ForeColor = System.Drawing.Color.White;
+            this.txtInputKey.Location = new System.Drawing.Point(17, 236);
+            this.txtInputKey.MaxLength = 50;
+            this.txtInputKey.Multiline = true;
+            this.txtInputKey.Name = "txtInputKey";
+            this.txtInputKey.Size = new System.Drawing.Size(262, 32);
+            this.txtInputKey.TabIndex = 9;
+            this.txtInputKey.Text = "***REMOVED***";
+            this.txtInputKey.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnConnect.ForeColor = System.Drawing.Color.White;
+            this.btnConnect.Location = new System.Drawing.Point(161, 282);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(93, 37);
+            this.btnConnect.TabIndex = 10;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.BackColor = System.Drawing.Color.DarkGreen;
+            this.txtResults.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtResults.ForeColor = System.Drawing.Color.White;
+            this.txtResults.Location = new System.Drawing.Point(178, 349);
+            this.txtResults.MaxLength = 50;
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.Size = new System.Drawing.Size(262, 57);
+            this.txtResults.TabIndex = 11;
+            this.txtResults.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtResults.Visible = false;
+            // 
+            // grpConnection
+            // 
+            this.grpConnection.Controls.Add(this.lblTitle2);
+            this.grpConnection.Controls.Add(this.btnConnect);
+            this.grpConnection.Controls.Add(this.lblTitle1);
+            this.grpConnection.Controls.Add(this.txtInputKey);
+            this.grpConnection.Controls.Add(this.btnRegister);
+            this.grpConnection.Controls.Add(this.lstHubs);
+            this.grpConnection.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.grpConnection.ForeColor = System.Drawing.Color.White;
+            this.grpConnection.Location = new System.Drawing.Point(12, 12);
+            this.grpConnection.Name = "grpConnection";
+            this.grpConnection.Size = new System.Drawing.Size(291, 331);
+            this.grpConnection.TabIndex = 12;
+            this.grpConnection.TabStop = false;
+            this.grpConnection.Text = "Connection";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnOceanM);
+            this.groupBox1.Controls.Add(this.btnColorloop);
+            this.groupBox1.Controls.Add(this.lblTitle3);
+            this.groupBox1.Controls.Add(this.btnNewColour);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(317, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(267, 331);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Color";
+            // 
+            // btnColorloop
+            // 
+            this.btnColorloop.FlatAppearance.BorderSize = 0;
+            this.btnColorloop.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnColorloop.ForeColor = System.Drawing.Color.White;
+            this.btnColorloop.Image = ((System.Drawing.Image)(resources.GetObject("btnColorloop.Image")));
+            this.btnColorloop.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnColorloop.Location = new System.Drawing.Point(47, 107);
+            this.btnColorloop.Name = "btnColorloop";
+            this.btnColorloop.Size = new System.Drawing.Size(77, 39);
+            this.btnColorloop.TabIndex = 9;
+            this.btnColorloop.Text = "Colorloop";
+            this.btnColorloop.UseVisualStyleBackColor = true;
+            this.btnColorloop.Click += new System.EventHandler(this.btnColorloop_Click);
+            // 
+            // btnOceanM
+            // 
+            this.btnOceanM.FlatAppearance.BorderSize = 0;
+            this.btnOceanM.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnOceanM.ForeColor = System.Drawing.Color.White;
+            this.btnOceanM.Image = ((System.Drawing.Image)(resources.GetObject("btnOceanM.Image")));
+            this.btnOceanM.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnOceanM.Location = new System.Drawing.Point(149, 107);
+            this.btnOceanM.Name = "btnOceanM";
+            this.btnOceanM.Size = new System.Drawing.Size(77, 39);
+            this.btnOceanM.TabIndex = 10;
+            this.btnOceanM.Text = "Ocean Mist";
+            this.btnOceanM.UseVisualStyleBackColor = true;
+            this.btnOceanM.Click += new System.EventHandler(this.btnOceanM_Click);
+            // 
+            // PCHueController
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(458, 166);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.panel1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(737, 436);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.lblCurrent);
+            this.Controls.Add(this.pnlCurrentSwatch);
+            this.Controls.Add(this.grpConnection);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "PCHueController";
             this.Text = "PCHueController";
+            this.grpConnection.ResumeLayout(false);
+            this.grpConnection.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,9 +260,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnlCurrentSwatch;
+        private System.Windows.Forms.Label lblCurrent;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.ListBox lstHubs;
+        private System.Windows.Forms.Label lblTitle1;
+        private System.Windows.Forms.Button btnNewColour;
+        private System.Windows.Forms.Label lblTitle2;
+        private System.Windows.Forms.Label lblTitle3;
+        private System.Windows.Forms.TextBox txtInputKey;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.GroupBox grpConnection;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnColorloop;
+        private System.Windows.Forms.Button btnOceanM;
     }
 }
 
--- PCHueController/PCHueController.cs
+++ PCHueController/PCHueController.cs
@@ -7,19 +7,118 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Q42.HueApi;
+using Q42.HueApi.Interfaces;
+using Q42.HueApi.Models.Bridge;
+using Q42.HueApi.ColorConverters;
+using Q42.HueApi.ColorConverters.OriginalWithModel;
 
 namespace PCHueController
 {
     public partial class PCHueController : Form
     {
+        public static string ip { get; set; }
+
+        static ILocalHueClient client = null;
+
         public PCHueController()
         {
             InitializeComponent();
+            GetBridgeIps();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public void clientConnection()
+        {
+            if (ip != null)
+            {
+                if (client == null)
+                {
+                    client = new LocalHueClient(ip);
+                }
+
+                else if (client.CheckConnection().Result == false)
+                {
+                    txtResults.BackColor = Color.Red;
+                    txtResults.Show();
+                    txtResults.Text = "Connection failed ensure you have internet connection.";
+                }
+            }
+            else
+            {
+                txtResults.BackColor = Color.Red;
+                txtResults.Show();
+                txtResults.Text = "Connection failed ensure you have internet connection.";
+            }
+        }
+
+        public async Task GetBridgeIps()
+        {
+            IBridgeLocator locator = new HttpBridgeLocator();
+            IEnumerable<LocatedBridge> bridgeIPs = await locator.LocateBridgesAsync(TimeSpan.FromSeconds(5));
+
+            foreach (var item in bridgeIPs)
+            {
+                string listFormat = string.Format("{0} ({1})", item.BridgeId, item.IpAddress);
+                lstHubs.Items.Add(listFormat);
+            }
+        }
+
+        private void lstHubs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ip = lstHubs.SelectedItem.ToString().Split('(')[1].Trim(')');
+            }
+
+            catch
+            {
+
+            }
+        }
+
+        private void btnNewColour_Click(object sender, EventArgs e)
         {
             LoadColorPick();
+        }
+
+
+        public async Task commandSender(LightCommand command)
+        {
+            await client.SendCommandAsync(command);
+        }
+
+        public async Task RegisterTask()
+        {
+            clientConnection();
+
+            if (client != null)
+            {
+                try
+                {
+                    var appKey = await client.RegisterAsync("PCHueController", "***REMOVED***");
+                    txtResults.Show();
+                    txtResults.Text = "App registered successfully." + Environment.NewLine + appKey;
+                    txtResults.BackColor = Color.Green;
+                    client.Initialize(appKey);
+                }
+                catch
+                {
+                    txtResults.BackColor = Color.Red;
+                    txtResults.Show();
+                    txtResults.Text = "App not registered, make sure button was pressed within 2 mins before registering.";
+                }
+            }
+            else
+            {
+                txtResults.BackColor = Color.Red;
+                txtResults.Show();
+                txtResults.Text = "Connection failed ensure you have internet connection.";
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            RegisterTask();
         }
 
         public void LoadColorPick()
@@ -30,11 +129,130 @@ namespace PCHueController
             // Allows the user to get help. (The default is false.)
             MyDialog.ShowHelp = true;
             // Sets the initial color select to the current text color.
-            MyDialog.Color = panel1.BackColor;
+            MyDialog.Color = pnlCurrentSwatch.BackColor;
 
             // Update the text box color if the user clicks OK 
             if (MyDialog.ShowDialog() == DialogResult.OK)
-                panel1.BackColor = MyDialog.Color;
+            {
+                clientConnection();
+
+                if (client != null)
+                {                      
+                    pnlCurrentSwatch.BackColor = MyDialog.Color;
+
+                    RGBColor convert = new RGBColor(MyDialog.Color.R, MyDialog.Color.G, MyDialog.Color.B);
+
+                    LightCommand command = new LightCommand();
+
+                    command.Effect = Effect.None;
+
+                    commandSender(command);
+
+                    command.Effect = null;
+
+                    command.TurnOn().SetColor(convert).Brightness = MyDialog.Color.A;
+
+                    commandSender(command);
+                }
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            clientConnection();
+
+            if (client != null)
+            {
+                try
+                {
+
+                    client.Initialize(txtInputKey.Text);
+
+                    if (client.CheckConnection().Result == true)
+                    {
+                        txtResults.Show();
+                        txtResults.Text = "Connected successfully.";
+                        txtResults.BackColor = Color.Green;
+
+                        try
+                        {
+                            IEnumerable<Light> lights = client.GetLightsAsync().Result;
+
+                            Color col = new Color();
+
+                            foreach (var item in lights)
+                            {
+                                string id= item.ModelId;
+                                col = ColorTranslator.FromHtml("#" + item.State.ToHex(id));
+                            }
+
+                            pnlCurrentSwatch.BackColor = col;
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+
+                    else
+                    {
+                        txtResults.BackColor = Color.Red;
+                        txtResults.Show();
+                        txtResults.Text = "Connection failed ensure you have choosen a device and that your key is correct.";
+                    }
+                }
+
+                catch
+                {
+                    txtResults.BackColor = Color.Red;
+                    txtResults.Show();
+                    txtResults.Text = "Connection failed ensure you have choosen a device and that your key is correct.";
+                }
+            }
+            else
+            {
+                txtResults.BackColor = Color.Red;
+                txtResults.Show();
+                txtResults.Text = "Connection failed ensure you have internet connection.";
+            }
+        }
+
+        private void btnColorloop_Click(object sender, EventArgs e)
+        {
+            clientConnection();
+
+            if (client != null)
+            {
+                var command = new LightCommand();
+                command.TurnOn().Effect = Effect.ColorLoop;
+                commandSender(command);
+            }
+
+            else
+            {
+                txtResults.BackColor = Color.Red;
+                txtResults.Show();
+                txtResults.Text = "Connection failed ensure you have internet connection.";
+            }
+        }
+
+        private void btnOceanM_Click(object sender, EventArgs e)
+        {
+            clientConnection();
+
+            if (client != null)
+            {
+                var command = new SceneCommand();
+                command.Scene = "Ocean Mist";
+                sceneSender(command);
+            }
+
+            else
+            {
+                txtResults.BackColor = Color.Red;
+                txtResults.Show();
+                txtResults.Text = "Connection failed ensure you have internet connection.";
+            }
         }
     }
 }
