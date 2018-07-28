04ba5be842e120e96a421dccb7eee3837b496c6a
Jstanford5216 <jstanford5216@gmail.com>
2018-07-28 17:35:16 +0100

    Forgot to add icon to resources but they are there now.

--- PCHueController/Properties/Resources.Designer.cs
+++ PCHueController/Properties/Resources.Designer.cs
@@ -63,6 +63,26 @@ namespace PCHueController.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Logo_Large {
+            get {
+                object obj = ResourceManager.GetObject("Logo_Large", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Logo_small {
+            get {
+                object obj = ResourceManager.GetObject("Logo_small", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Off {
             get {
                 object obj = ResourceManager.GetObject("Off", resourceCulture);
